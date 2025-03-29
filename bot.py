1e41e587af58565c3d07fe62f3718269eb127517
sashatsyhanov <alexandertsyhanov@gmail.com>
2025-03-29 17:12:50 +0300

    Fix NameError by adding StatesGroup import

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters.state import State, StatesGroup  # Добавляем StatesGroup
 from aiogram.utils import executor
 import os
 import aiosqlite
